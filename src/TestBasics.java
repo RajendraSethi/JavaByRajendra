6ca26cebab593ef75a364e3acc817ede78d4d638
Rajendra Sethi <sethirajendra07@gmail.com>
2021-08-13 14:38:05 +0530

    file commit

---
+++ src/TestBasics.java
@@ -0,0 +1,12 @@
+
+public class TestBasics {
+	 public static void main(String[] args) {
+		System.out.println("Hi");
+		m1();
+		
+	}
+	 static void m1() {
+		 System.out.println("m1");
+		 main(new String[1]);
+	 }
+}
